0bb5f044d758bf1dd131a690a7cd2d200cfbec41
nicknish09 <nnmarques97@gmail.com>
2019-11-16 12:31:57 -0300

    process manager criado

---
+++ process_manager.rb
@@ -0,0 +1,10 @@
+class ProcessManager
+  real_time_queue = []
+  user_queue = []
+  first_queue = []
+  second_queue = []
+  third_queue = []
+  main_queue = []
+  in_execution = {}
+  lastPID = 0
+end
