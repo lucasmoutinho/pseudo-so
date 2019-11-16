b181346797fd32119cc99eee8ab35284091e1b3b
nicknish09 <nnmarques97@gmail.com>
2019-11-16 16:11:35 -0300

    process atualizado

--- process.rb
+++ process.rb
@@ -1,15 +1,15 @@
-class Process
+class SOProcess
   @@io_request_types = %i[scanner printer modem sata none]
 
   def initialize(process)
-    @created_at = Time.now
-    @priority = process.priority
-    @cpu_time = process.cpu_time
-    @memory_blocks = process.memory_blocks
-    @printer_code = process.printer_code
-    @scanner_request = process.scanner_request
-    @modem_request = process.modem_request
-    @sata_code = process.sata_code
+    @created_at = process[0]
+    @priority = process[1] #process.priority
+    @cpu_time = process[2] #process.cpu_time
+    @memory_blocks = process[3] #process.memory_blocks
+    @printer_code = process[4] #process.printer_code
+    @scanner_request = process[5] #process.scanner_request
+    @modem_request = process[6] #process.modem_request
+    @sata_code = process[7] #process.sata_code
     @offset = nil
     @pid = nil
     @times_executed = 0
