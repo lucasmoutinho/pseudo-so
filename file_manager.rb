9e2b05ce333d9afa6d8c6050c72f7a4ce7f41190
nicknish09 <nnmarques97@gmail.com>
2019-11-17 17:23:34 -0300

    file manager deletar duvidoso

--- file_manager.rb
+++ file_manager.rb
@@ -27,50 +27,64 @@ class FileManager
     end
 
     def create_file(name, size, creator)
+        puts "name: #{name}"
         offset = nil
         available = 0
         if(files.any? {|file| file.name == name})
             @log.append({
                 status: 'Falha',
-                mensagem: 'O processo nao criou o arquivo (Arquivo ja existe no disco)'
+                mensagem: "O processo P#{creator} nao criou o arquivo (Arquivo ja existe no disco)"
             })
             return
         end
-        blocks_quantity.times {|i|
+        # puts "blocks_q: #{blocks_quantity}"
+        blocks_quantity.times do |i|
             block = @disc[i]
             if(block == 0)
                 available += 1
+                puts "fff: #{available == size} size: #{size}"
                 if(available == size)
                     offset = i - available + 1
                     if offset != 0
-                        @disc[offset % offset + available] = name
+                        size.times do |j|
+                            @disc[offset % offset + available + j] = name
+                            offset += 1
+                        end
+                    else
+                        size.times do |j|
+                            @disc[available + j] = name
+                            offset += 1
+                        end
                     end
                     file = File.new([name, offset, size], creator)
                     @files.append(file)
                     @log.append({
                         status: 'Sucesso',
-                        mensagem: "O processo criou o arquivo"
+                        mensagem: "O processo P#{creator} criou o arquivo"
                     })
-                    # return
+                    return
                 end
             else
                 available = 0
             end
-        }
+        end
         @log.append({
             status: "Falha",
-            mensagem: "O processo nao criou o arquivo (sem espaco livre)"
+            mensagem: "O processo P#{creator} nao criou o arquivo (sem espaco livre)"
         })
     end
 
     def delete_file(file)
-        @disc[file.start_block % file.start_block + file.size] = file.size * [0]
+        file.size.times do |i|
+            @disc[file.start_block + i] = 0
+        end
     end
 
     def operate_process(process)
         ops = @operations.select { |op| op.processId == process.pid }
         puts "ops: #{ops}"
         ops.each do |op|
+            puts "opcode: #{op.opcode}"
             if(op.opcode == 0)
                 create_file(op.file, op.size, process.pid)
             else
@@ -80,18 +94,18 @@ class FileManager
                         delete_file(file)
                         @log.append({
                             status: 'sucesso',
-                            mensagem: "O processo deletou o arquivo"
+                            mensagem: "O processo P#{process.pid} deletou o arquivo"
                         })
                     else
                         @log.append({
                             status: 'Falha',
-                            mensagem: 'O processo nao pode deletar o arquivo'
+                            mensagem: "O processo #{process.pid} nao pode deletar o arquivo"
                         })
                     end
                 else
                     @log.append({
                         status: "Falha",
-                        mensagem: 'O processo nao pode deletar o arquivo'
+                        mensagem: "O processo P#{process.pid} nao pode deletar o arquivo"
                     })
                 end
             end
--- main.rb
+++ main.rb
@@ -44,7 +44,7 @@ def main
 
   aux_filesystem_array.each do |operation|
     aux_operation = FileOperation.new(operation.split(','))
-    puts "aux_operation: #{aux_operation.file}"
+    puts "aux_operation: #{aux_operation.opcode}"
     filesystem_manager.operations.push aux_operation
   end
 
